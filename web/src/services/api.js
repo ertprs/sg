d3ebfbba030fa4a01fb226c16c3948ca753cb388
Alvaro Ferreira <alvarojfjunior@gmail.com>
2020-10-14 19:55:39 -0300

    remove db

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -5,10 +5,11 @@ const userUrl = String(window.location.href).slice(0, 14);
 var baseURL = '';
 if (userUrl === 'http://192.168')
   baseURL = 'http://192.168.1.1:3333'
-else {
+else if (userUrl === 'http://localho')
+  baseURL = 'http://localhost:3333'
+else
   baseURL = 'http://177.36.223.44:3333';
-  //baseURL = 'http://localhost:3333';
-}
+
 
 console.log(baseURL)
 
