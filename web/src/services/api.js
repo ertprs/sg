d8809dcb2ea5430d734f8766cca3f3bc454f3434
Alvaro Ferreira <alvarojfjunior@gmail.com>
2020-10-14 16:51:55 -0300

    urli

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const baseURL = 'https://sgcog.herokuapp.com';
-const baseURL = 'http://localhost:3333';
+const baseURL = 'http://192.168.1.1:3333';
 //const baseURL = 'http://177.36.223.44:3333';
 
 const api = axios.create({
