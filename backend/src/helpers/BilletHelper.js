f0048782b10cf3e984fe6925e153f25d3527ae78
Alvaro Ferreira <alvarojfjunior@gmail.com>
2020-10-26 18:50:04 -0200

    asaas

--- backend/src/helpers/BilletHelper.js
+++ backend/src/helpers/BilletHelper.js
@@ -6,18 +6,19 @@ const emitBillet = async (id) => {
   try {
     const asaasHeader = {
       headers: {
-        access_token: '97e802fc8baf605139013c728b6178ff5ff2c007fcac83305a442b9431ff57fb'
+        "Content-Type": "application/json",
+        "access_token": '97e802fc8baf605139013c728b6178ff5ff2c007fcac83305a442b9431ff57fb'
       }
     };
 
     const asaasBody = {
-      customer: "16317475",
-      billingType: "BOLETO",
-      dueDate: "2017-06-10",
-      value: 1,
-      description: "Pedido 056984",
-      externalReference: "056984", //campo para busca
-      postalService: false
+      "customer": "16317475",
+      "billingType": "BOLETO",
+      "dueDate": "2017-06-10",
+      "value": 1,
+      "description": "Pedido 056984",
+      "externalReference": "056984", //campo para busca
+      "postalService": false
     }
 
     const res = await asaas.api.post('payments', asaasBody, asaasHeader);
