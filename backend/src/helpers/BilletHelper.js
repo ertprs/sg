ee619b1961d4b59874b5cccd80c9617054823deb
Alvaro Ferreira <alvarojfjunior@gmail.com>
2020-10-27 11:42:46 -0200

    asaas

--- backend/src/helpers/BilletHelper.js
+++ backend/src/helpers/BilletHelper.js
@@ -15,19 +15,15 @@ const emitBillet = async (billetId, client, billetTotal, attendance, dtDue) => {
     };
 
     const asaasBody = {
-      "customer": "16317475", //generate or create Assas code by client id
+      "customer": getAsaasCodeByClientId(client),
       "billingType": "BOLETO",
-      "dueDate": moment(dtDue).format('yyyy-DD-mm'),
+      "dueDate": moment(dtDue, 'DD/MM/YYYY').format('yyyy-DD-mm'),
       "value": myFormat.strValueToFloat(billetTotal),
-      "description": "Cobranca " + String(attendance),
+      "description": "Atendimento " + String(attendance),
       "externalReference": String(billetId), //campo para busca
       "postalService": false
     }
-
-    console.log(asaasBody)
-
-    const res = {}//await asaas.api.post('payments', asaasBody, asaasHeader);
-
+    const res = await asaas.api.post('payments', asaasBody, asaasHeader);
     return res.data;
   } catch (error) {
     return error.message;
@@ -35,6 +31,40 @@ const emitBillet = async (billetId, client, billetTotal, attendance, dtDue) => {
 }
 
 
+const getAsaasCodeByClientId = async clientId => {
+  try {
+    const clientRes = await connection('clients')
+      .where('id', '=', clientId)
+      .select('*')
+      .first();
+
+    if (clientRes.asaas_code)
+      return clientRes.asaas_code;
+
+    console.log('não existe')
+    const asaasHeader = {
+      headers: {
+        "Content-Type": "application/json",
+        "access_token": '97e802fc8baf605139013c728b6178ff5ff2c007fcac83305a442b9431ff57fb'
+      }
+    };
+    const asaasBody = {
+      "name": clientRes.name,
+      "cpfCnpj": clientRes.document,
+      "email": clientRes.email_additional ? clientRes.email_additional : clientRes.email,
+      "mobilePhone": clientRes.phone_additional ? phone_additional : clientRes.cellphone,
+    }
+    const asaasRes = await asaas.api.post('customers', asaasBody, asaasHeader);
+    const updateClientRes = await connection('clients').where('id', '=', clientId).update({ asaas_code: asaasRes.data.id });
+    return asaasRes.data.id;
+  } catch (error) {
+    return error.message;
+  }
+
+}
+
+
 module.exports = {
-  emitBillet
+  emitBillet,
+  getAsaasCodeByClientId
 }
